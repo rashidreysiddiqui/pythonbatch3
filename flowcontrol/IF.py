30db262b8ae074fa86437643a18b42f69d2b895d
rashid <rashidsiddqui07@gmail.com>
2020-05-07 22:27:51 +0530

    print statement

---
+++ flowcontrol/IF.py
@@ -0,0 +1,12 @@
+fnum=int(input("enter first number"))
+snum=int(input("enter second "))
+
+if(fnum>snum):
+    print("first")
+    print("will it come?")
+    print("what about me.")
+    print("should i Come")
+else:
+    print("second")
+    print("hi there its me")
+print("lets try this !!")
---
+++ prints.py
@@ -0,0 +1,18 @@
+
+# print(10,20,30,40,50,end=" | ",sep=" : ")
+# print(60,70,80,90,100,sep=" - ")
+# print("welcome")
+# formatted string
+# %i-> int
+# %d-> int 
+#  %f-> float
+# %s-> str
+# name="rashid"
+# fee=1000
+# print("%s fee is %i"%(name,fee))
+# Replacement operator
+# {}
+# name="rashid"
+# fee=1000
+# course="python"
+# print("{x} fee is {y} for {z}".format(y=fee,x=name,z=course))
